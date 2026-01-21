4da2de330c2409773408b81a8da908cc26fe67e8
Guillaume Huysmans <ghuysmans99@gmail.com>
2026-01-21 16:44:44 +0100

    Pass the context to the seek callback
    This adds a 'ctx parameter to the archive type.

--- archive/ArchiveLow.ml
+++ archive/ArchiveLow.ml
@@ -9,7 +9,7 @@
 
     @author Sylvain Le Gall *)
 
-type 'a archive
+type ('cap, 'ctx) archive
 type filename = string
 type error_code = int
 
@@ -25,7 +25,7 @@ type 'a skip_callback = 'a -> int -> int
 (* archive_read_open2 close callback *)
 type 'a close_callback = 'a -> unit
 
-type seek_callback = int -> Unix.seek_command -> int
+type 'a seek_callback = 'a -> int -> Unix.seek_command -> int
 
 exception AEnd_of_file
 exception AFailure of error_code * string
@@ -53,29 +53,29 @@ module Entry = struct
 end
 
 module Read = struct
-  type t = [ `Read ] archive
+  type 'ctx t = ([ `Read ], 'ctx) archive
 
-  external create : unit -> t = "caml_archive_read_create"
+  external create : unit -> _ t = "caml_archive_read_create"
   (** archive_read_new *)
 
-  external support_filter_all : t -> unit
+  external support_filter_all : _ t -> unit
     = "caml_archive_read_support_filter_all"
   (** archive_read_support_filter_all *)
 
-  external support_format_all : t -> unit
+  external support_format_all : _ t -> unit
     = "caml_archive_read_support_format_all"
   (** archive_read_support_compress_all *)
 
-  external open_filename : t -> filename -> int -> unit
+  external open_filename : _ t -> filename -> int -> unit
     = "caml_archive_read_open_filename"
   (** archive_read_open_filename *)
 
-  external set_seek_callback : t -> seek_callback -> unit
+  external set_seek_callback : 'a t -> 'a seek_callback -> unit
     = "caml_archive_read_set_seek_callback"
   (** archive_read_set_seek_callback *)
 
   external open2 :
-    t ->
+    'b t ->
     ('a, 'b) open_callback ->
     'b read_callback ->
     'b skip_callback ->
@@ -84,10 +84,10 @@ module Read = struct
     unit = "caml_archive_read_open2_bytecode" "caml_archive_read_open2_native"
   (** archive_read_open2 *)
 
-  external next_header2 : t -> Entry.t -> bool
+  external next_header2 : _ t -> Entry.t -> bool
     = "caml_archive_read_next_header2"
 
-  external data_skip : t -> unit = "caml_archive_read_data_skip"
-  external data : t -> string -> int -> int -> int = "caml_archive_read_data"
-  external close : t -> unit = "caml_archive_read_close"
+  external data_skip : _ t -> unit = "caml_archive_read_data_skip"
+  external data : _ t -> string -> int -> int -> int = "caml_archive_read_data"
+  external close : _ t -> unit = "caml_archive_read_close"
 end
